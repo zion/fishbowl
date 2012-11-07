52d8b4d6e6f6e05284acae04ccc370b3951cc4bc
James Thompson <james@plainprograms.com>
2012-11-07 15:10:16 -0700

    Fleshed out spec and implementation for Fishbowl::Objects::BaseObject.

--- lib/fishbowl/objects/base_object.rb
+++ lib/fishbowl/objects/base_object.rb
@@ -1,5 +1,30 @@
 module Fishbowl::Objects
   class BaseObject
+    attr_accessor :ticket
+
+  protected
+
+    def send_request(request)
+      Fishbowl::Connection.send(build_request(request))
+    end
+
+  private
+
+    def build_request(request)
+      Nokogiri::XML::Builder.new do |xml|
+        xml.FbiXml {
+          if @ticket.nil?
+            xml.Ticket
+          else
+            xml.Ticket @ticket
+          end
+
+          xml.FbiMsgsRq {
+            xml.send(request)
+          }
+        }
+      end
+    end
 
   end
 end
--- spec/objects/base_object_spec.rb
+++ spec/objects/base_object_spec.rb
@@ -1,5 +1,60 @@
 require 'spec_helper'
 
 describe Fishbowl::Objects::BaseObject do
-  pending
+  let(:ticket) { "thisisasample" }
+  let(:base_object) { Fishbowl::Objects::BaseObject.new }
+
+  let(:empty_ticket_builder) do
+    Nokogiri::XML::Builder.new do |xml|
+      xml.FbiXml {
+        xml.Ticket
+
+        xml.FbiMsgsRq {
+          xml.SampleRequest
+        }
+      }
+    end
+  end
+
+  let(:ticket_builder) do
+    Nokogiri::XML::Builder.new do |xml|
+      xml.FbiXml {
+        xml.Ticket ticket
+
+        xml.FbiMsgsRq {
+          xml.SampleRequest
+        }
+      }
+    end
+  end
+
+  context "Protected Methods" do
+    describe "#send_request" do
+
+    end
+  end
+
+  context "Private Methods" do
+    describe "#build_request" do
+      it "should build a request document" do
+        base_object.send(:build_request, "SampleRequest").to_xml.should eq(empty_ticket_builder.to_xml)
+      end
+
+      context "when ticket is empty" do
+        it "should return an empty Nokogiri::XML::Builder" do
+          base_object.send(:build_request, "SampleRequest").to_xml.should eq(empty_ticket_builder.to_xml)
+        end
+      end
+
+      context "when ticket is set" do
+        before :each do
+          base_object.ticket = ticket
+        end
+
+        it "should return the ticket wrapped in a Nokogiri::XML::Builder" do
+          base_object.send(:build_request, "SampleRequest").to_xml.should eq(ticket_builder.to_xml)
+        end
+      end
+    end
+  end
 end
