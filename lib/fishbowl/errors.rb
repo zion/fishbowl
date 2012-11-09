9b5230d08b4f6beb649fb11d2687a461b94fcc9d
James Thompson <james@plainprograms.com>
2012-11-09 14:24:16 -0700

    Added spec and implementation for Fishbowl::Errors.confirm_success_or_raise.

--- lib/fishbowl/errors.rb
+++ lib/fishbowl/errors.rb
@@ -3,4 +3,10 @@ module Fishbowl::Errors
   class MissingHost < ArgumentError; end;
   class MissingUsername < ArgumentError; end;
   class MissingPassword < ArgumentError; end;
+
+  class StatusError < RuntimeError; end;
+
+  def self.confirm_success_or_raise(code, message)
+    code.to_i == 1000 ? true : raise(StatusError, message)
+  end
 end
