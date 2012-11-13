a47493a54c904816bfa91e63986765e61c04164f
James Thompson <james@plainprograms.com>
2012-11-13 15:23:15 -0700

    Added spec and implementation for AddSOItem request.

--- lib/fishbowl/requests.rb
+++ lib/fishbowl/requests.rb
@@ -3,12 +3,9 @@ module Fishbowl
     def self.add_inventory(options = {})
       options = options.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
 
-      raise ArgumentError if options[:part_number].nil?
-      raise ArgumentError if options[:quantity].nil?
-      raise ArgumentError if options[:uom_id].nil?
-      raise ArgumentError if options[:cost].nil?
-      raise ArgumentError if options[:location_tag_number].nil?
-      raise ArgumentError if options[:tag_number].nil?
+      %w{part_number quantity uom_id cost location_tag_number tag_number}.each do |required_field|
+        raise ArgumentError if options[required_field.to_sym].nil?
+      end
 
       raise ArgumentError unless options[:tracking].nil? || options[:tracking].is_a?(Fishbowl::Object::Tracking)
 
@@ -28,6 +25,41 @@ module Fishbowl
       end
 
       Fishbowl::Objects::BaseObject.new.send_request(request, 'AddInventoryRs')
+    end
+
+    def self.add_sales_order_item(options = {})
+      options = options.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+
+      %w{order_number id product_number sales_order_id description taxable
+         quantity product_price total_price uom_code item_type status
+         quickbooks_class_name new_item_flag}.each do |required_field|
+        raise ArgumentError if options[required_field.to_sym].nil?
+      end
+
+      request = Nokogiri::XML::Builder.new do |xml|
+        xml.request {
+          xml.AddSOItemRq {
+            xml.OrderNum options[:order_number]
+            xml.SalesOrderItem {
+              xml.ID options[:id]
+              xml.ProductNumber options[:product_number]
+              xml.SOID options[:sales_order_id]
+              xml.Description options[:description]
+              xml.Taxable options[:taxable]
+              xml.Quantity options[:quantity]
+              xml.ProductPrice options[:product_price]
+              xml.TotalPrice options[:total_price]
+              xml.UOMCode options[:uom_code]
+              xml.ItemType options[:item_type]
+              xml.Status options[:status]
+              xml.QuickBooksClassName options[:quickbooks_class_name]
+              xml.NewItemFlag options[:new_item_flag]
+            }
+          }
+        }
+      end
+
+      Fishbowl::Objects::BaseObject.new.send_request(request, 'AddSOItemRs')
     end
   end
 end
