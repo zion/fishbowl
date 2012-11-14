fa16aeed56d40374e93c59d97c52c75afd162aed
James Thompson <james@plainprograms.com>
2012-11-14 13:15:20 -0700

    Added spec for GetShipList request.

---
+++ spec/requests/get_ship_list_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe Fishbowl::Requests do
+  describe "#get_ship_list" do
+    before :each do
+      mock_tcp_connection
+      mock_login_response
+      Fishbowl::Connection.connect(host: 'localhost')
+      Fishbowl::Connection.login(username: 'johndoe', password: 'secret')
+    end
+
+    let(:connection) { FakeTCPSocket.instance }
+
+    let(:valid_options) {
+      #TODO Identify valid options
+    }
+
+    it "sends proper request" do
+      mock_the_response(expected_response)
+      Fishbowl::Requests.get_ship_list(valid_options)
+      connection.last_write.should be_equivalent_to(expected_request)
+    end
+
+    def expected_request
+      request = Nokogiri::XML::Builder.new do |xml|
+        xml.FbiXml {
+          xml.Ticket
+          xml.FbiMsgsRq {
+            xml.GetShipListRq {
+              #TODO Figure out what goes here!
+            }
+          }
+        }
+      end
+
+      request.to_xml
+    end
+
+    def expected_response
+      Nokogiri::XML::Builder.new do |xml|
+        xml.response {
+          xml.GetShipListRs(statusCode: '1000', statusMessage: "Success!") {
+            #TODO figure out what goes here!
+          }
+        }
+      end
+    end
+  end
+end
