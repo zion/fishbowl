14336dd0b66eeb0d4704a51a7f1987fc228d671a
James Thompson <james@plainprograms.com>
2012-11-15 11:10:19 -0700

    Added spec for LocationQuery request.

---
+++ spec/requests/location_query_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe Fishbowl::Requests do
+  describe "#location_query" do
+    before :each do
+      mock_tcp_connection
+      mock_login_response
+      Fishbowl::Connection.connect(host: 'localhost')
+      Fishbowl::Connection.login(username: 'johndoe', password: 'secret')
+    end
+
+    let(:connection) { FakeTCPSocket.instance }
+
+    let(:valid_options) {
+      #TODO figure out what valid options are
+    }
+
+    it "sends proper request" do
+      mock_the_response(expected_response)
+      Fishbowl::Requests.location_query(valid_options)
+      connection.last_write.should be_equivalent_to(expected_request)
+    end
+
+    def expected_request
+      request = Nokogiri::XML::Builder.new do |xml|
+        xml.FbiXml {
+          xml.Ticket
+          xml.FbiMsgsRq {
+            xml.LocationQueryRq {
+              #TODO figure out what goes here!
+            }
+          }
+        }
+      end
+
+      request.to_xml
+    end
+
+    def expected_response
+      Nokogiri::XML::Builder.new do |xml|
+        xml.response {
+          xml.LocationQueryRs(statusCode: '1000', statusMessage: "Success!") {
+            #TODO figure out what goes here!
+          }
+        }
+      end
+    end
+  end
+end
