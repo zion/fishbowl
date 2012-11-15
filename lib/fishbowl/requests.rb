15c92979e09c27a3fbb5df1bdce2f80e14074f9e
James Thompson <james@plainprograms.com>
2012-11-15 14:32:16 -0700

    Refactored lib/fishbowl/requests.rb to use a Dir.glob to include all request implementations.

--- lib/fishbowl/requests.rb
+++ lib/fishbowl/requests.rb
@@ -1,6 +1,5 @@
 module Fishbowl; module Requests; end; end
 
-require 'fishbowl/requests/add_inventory'
-require 'fishbowl/requests/add_sales_order_item'
-require 'fishbowl/requests/adjust_inventory'
-require 'fishbowl/requests/get_carrier_list'
+Dir.glob(File.join(File.expand_path("../requests/", __FILE__), "**.rb")).each do |file|
+  require file
+end
