f73df2994a006ca274fbc97ec0ab2e9c72aea069
James Thompson <james@plainprograms.com>
2012-11-15 11:12:05 -0700

    Added spec for MakePayment request.

---
+++ spec/requests/make_payment_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe Fishbowl::Requests do
+  describe "#make_payment" do
+    before :each do
+      mock_tcp_connection
+      mock_login_response
+      Fishbowl::Connection.connect(host: 'localhost')
+      Fishbowl::Connection.login(username: 'johndoe', password: 'secret')
+    end
+
+    let(:connection) { FakeTCPSocket.instance }
+
+    let(:valid_options) {
+      #TODO figure out valid options
+    }
+
+    it "sends proper request" do
+      mock_the_response(expected_response)
+      Fishbowl::Requests.make_payment(valid_options)
+      connection.last_write.should be_equivalent_to(expected_request)
+    end
+
+    def expected_request
+      request = Nokogiri::XML::Builder.new do |xml|
+        xml.FbiXml {
+          xml.Ticket
+          xml.FbiMsgsRq {
+            xml.MakePaymentRq {
+              xml.Payment #TODO mock Payment object
+              xml.ProcessPayment true
+            }
+          }
+        }
+      end
+
+      request.to_xml
+    end
+
+    def expected_response
+      Nokogiri::XML::Builder.new do |xml|
+        xml.response {
+          xml.MakePaymentRs(statusCode: '1000', statusMessage: "Success!") {
+            #TODO figure out what goes here!
+          }
+        }
+      end
+    end
+  end
+end
