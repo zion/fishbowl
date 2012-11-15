826c412355a915386c2e2e7489a83cdc29762faf
James Thompson <james@plainprograms.com>
2012-11-15 11:16:59 -0700

    Added spec for PartGet request. [ci skip]

---
+++ spec/requests/get_part_information_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe Fishbowl::Requests do
+  describe "#get_part_information" do
+    before :each do
+      mock_tcp_connection
+      mock_login_response
+      Fishbowl::Connection.connect(host: 'localhost')
+      Fishbowl::Connection.login(username: 'johndoe', password: 'secret')
+    end
+
+    let(:connection) { FakeTCPSocket.instance }
+
+    it "sends proper request" do
+      mock_the_response(expected_response)
+      Fishbowl::Requests.get_part_information("B203")
+      connection.last_write.should be_equivalent_to(expected_request)
+    end
+
+    def expected_request(options = {})
+      request = Nokogiri::XML::Builder.new do |xml|
+        xml.FbiXml {
+          xml.Ticket
+          xml.FbiMsgsRq {
+            xml.PartGetRq {
+              xml.PartNum "B203"
+            }
+          }
+        }
+      end
+
+      request.to_xml
+    end
+
+    def expected_response
+      Nokogiri::XML::Builder.new do |xml|
+        xml.response {
+          xml.PartGetRs(statusCode: '1000', statusMessage: "Success!") {
+            #TODO figure out what goes here!
+          }
+        }
+      end
+    end
+  end
+end
